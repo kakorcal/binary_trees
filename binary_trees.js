53088d53a6eddd66e04b6c143f5a6c225a1ca679
Kenneth Korcal <kakorcal@gmail.com>
2016-06-23 05:32:06 -0700

    adding tests. ensuring test runs properly

---
+++ binary_trees.js
@@ -0,0 +1,47 @@
+function BinTree(){
+  this.root = null;
+}
+
+function Node(value, left=null, right=null){
+  this.value = value;
+  this.left = left;
+  this.right = right;
+}
+
+BinTree.prototype.insertIteratively = function(value){
+
+};
+
+BinTree.prototype.insertRecursively = function(value,current){
+
+};
+
+BinTree.prototype.containsIteratively = function(value){
+
+};
+
+BinTree.prototype.containsRecursively = function(value,current){
+
+};
+
+BinTree.prototype.breadthFirstSearch = function() {
+
+};
+
+BinTree.prototype.DFSPreOrder = function() {
+
+};
+
+BinTree.prototype.DFSInOrder = function() {
+
+};
+
+BinTree.prototype.DFSPostOrder = function() {
+
+};
+
+BinTree.prototype.size = function() {
+
+};
+
+module.exports = {BinTree,Node};
